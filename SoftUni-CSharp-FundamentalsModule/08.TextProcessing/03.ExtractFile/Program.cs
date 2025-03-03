af7d5c8f7f9eabbac46232130981eac7636b6128
Krasimir Naydenov <krasi.photography@gmail.com>
2025-03-03 18:39:09 +0200

    Refactored: Removed top-level statements from all exercises in 08.TextProcessing

---
+++ SoftUni-CSharp-FundamentalsModule/08.TextProcessing/03.ExtractFile/Program.cs
@@ -0,0 +1,11 @@
+namespace _U
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
